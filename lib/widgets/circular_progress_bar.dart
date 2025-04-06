9bf02599621424a6301b75a65a5ca84a337252a9
PC <victorcibas@protonmail.com>
2025-04-06 14:52:56 -0300

    Mudança na "barra de progresso", anteriormente um circulo que se preenchia da direita pra esquerda agora se preencher de cima para baixo com uma animação para lembrar água.

--- lib/widgets/circular_progress_bar.dart
+++ lib/widgets/circular_progress_bar.dart
@@ -13,7 +13,7 @@ class CircularProgressBar extends StatefulWidget {
     super.key,
     required this.progress,
     this.max = 100,
-    this.progressColor = Colors.green,
+    this.progressColor = Colors.blueAccent,
     this.backgroundColor = Colors.grey,
     this.strokeWidth = 20.0,
     this.animationDuration = const Duration(milliseconds: 1000),
@@ -37,51 +37,37 @@ class _CircularProgressBarState extends State<CircularProgressBar>
     super.initState();
     _controller = AnimationController(
       vsync: this,
-      duration: widget.animationDuration,
-    );
+      duration: const Duration(seconds: 2),
+    )..repeat();
+
     _animation = Tween<double>(
       begin: 0,
-      end: _progressFraction,
-    ).animate(_controller)
-      ..addListener(() => setState(() {}));
-    _controller.forward();
+      end: 2 * pi,
+    ).animate(_controller);
+
+    _controller.addListener(() {
+      setState(() {});
+    });
   }
 
   @override
   void didUpdateWidget(covariant CircularProgressBar oldWidget) {
     super.didUpdateWidget(oldWidget);
-
-    if (oldWidget.progress != widget.progress || oldWidget.max != widget.max) {
-      _oldProgress = _animation.value;
-
-      _controller
-        ..reset()
-        ..duration = widget.animationDuration;
-
-      _animation = Tween<double>(
-        begin: _oldProgress,
-        end: _progressFraction,
-      ).animate(
-        CurvedAnimation(parent: _controller, curve: Curves.decelerate),
-      )..addListener(() => setState(() {}));
-
-      _controller.forward();
-    }
   }
 
   @override
   Widget build(BuildContext context) {
     return CustomPaint(
-      painter: _CircularProgressPainter(
-        progress: _animation.value,
+      painter: _WaterWavePainter(
+        wavePhase: _animation.value,
+        progress: _progressFraction,
         progressColor: widget.progressColor,
         backgroundColor: widget.backgroundColor,
-        strokeWidth: widget.strokeWidth,
       ),
-        child: SizedBox(
-          width: 200,
-          height: 200,
-        ),
+      child: const SizedBox(
+        width: 200,
+        height: 200,
+      ),
     );
   }
 
@@ -92,43 +78,58 @@ class _CircularProgressBarState extends State<CircularProgressBar>
   }
 }
 
-class _CircularProgressPainter extends CustomPainter {
+class _WaterWavePainter extends CustomPainter {
   final double progress;
+  final double wavePhase;
   final Color progressColor;
   final Color backgroundColor;
-  final double strokeWidth;
 
-  const _CircularProgressPainter({
+  _WaterWavePainter({
     required this.progress,
+    required this.wavePhase,
     required this.progressColor,
     required this.backgroundColor,
-    required this.strokeWidth,
   });
 
   @override
   void paint(Canvas canvas, Size size) {
-    final Paint bgPaint = Paint()
-      ..color = backgroundColor
-      ..strokeWidth = strokeWidth
-      ..style = PaintingStyle.stroke
-      ..strokeCap = StrokeCap.round;
+    final radius = min(size.width, size.height) / 2;
+    final center = Offset(size.width / 2, size.height / 2);
+    final circleRect = Rect.fromCircle(center: center, radius: radius);
 
-    final Paint fgPaint = Paint()
-      ..color = progressColor
-      ..strokeWidth = strokeWidth
-      ..style = PaintingStyle.stroke
-      ..strokeCap = StrokeCap.round;
+    canvas.save();
+    canvas.clipPath(Path()..addOval(circleRect));
 
-    final double radius = (min(size.width, size.height) - strokeWidth) / 2;
-    final Offset center = Offset(size.width / 2, size.height / 2);
-    final Rect rect = Rect.fromCircle(center: center, radius: radius);
+    final backgroundPaint = Paint()..color = backgroundColor;
+    canvas.drawCircle(center, radius, backgroundPaint);
 
-    canvas.drawArc(rect, 0, 2 * pi, false, bgPaint);
-    canvas.drawArc(rect, -pi / 2, 2 * pi * progress, false, fgPaint);
+    final path = Path();
+    double waveHeight = 8;
+    double waveLength = size.width;
+    double baseHeight = size.height * (1 - progress);
+
+    path.moveTo(0, baseHeight);
+
+    for (double x = 0; x <= size.width; x++) {
+      double y = waveHeight * sin((2 * pi / waveLength) * x + wavePhase) + baseHeight;
+      path.lineTo(x, y);
+    }
+
+    path.lineTo(size.width, size.height);
+    path.lineTo(0, size.height);
+    path.close();
+
+    final wavePaint = Paint()..color = progressColor.withOpacity(0.6);
+    canvas.drawPath(path, wavePaint);
+
+    canvas.restore();
   }
 
   @override
-  bool shouldRepaint(covariant _CircularProgressPainter oldDelegate) {
-    return oldDelegate.progress != progress;
+  bool shouldRepaint(covariant _WaterWavePainter oldDelegate) {
+    return oldDelegate.progress != progress ||
+        oldDelegate.wavePhase != wavePhase ||
+        oldDelegate.progressColor != progressColor ||
+        oldDelegate.backgroundColor != backgroundColor;
   }
 }
